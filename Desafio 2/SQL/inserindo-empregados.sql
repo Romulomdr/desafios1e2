bc010afb844a5520cf8018454e6637eaf1753a70
romulomdr <romulomatheus31@gmail.com>
2024-07-14 17:29:04 -0300

    fix SQL employe for readme

--- Desafio 2/SQL/inserindo-empregados.sql
+++ Desafio 2/SQL/inserindo-empregados.sql
@@ -1,5 +1,5 @@
 INSERT INTO employee (name, email) VALUES ('Romulo M', 'romulo@example.com');
 INSERT INTO employee (name, email) VALUES ('Alex S', 'alex@example.com');
-INSERT INTO employee (name, email) VALUES ('Jorge D', 'jorge@example.com');
+INSERT INTO employee (name, email) VALUES ('Pedro D', 'pedro@example.com');
 INSERT INTO employee (name, email) VALUES ('Maria C', 'maria@example.com');
 INSERT INTO employee (name, email) VALUES ('Larissa R', 'larissa@example.com');
